1fbebff7f46e491c9362d91fcdc285a63f205093
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-03-05 00:14:26 +0300

    HW_task10

--- HW_task10/Program.cs
+++ HW_task10/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine());
+
+string num = Convert.ToString(a);
+
+Console.WriteLine ("Вторая цифра трехзначного числа " + num[1]);
